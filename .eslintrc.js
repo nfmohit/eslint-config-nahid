a908b77f3bdd0e2dedc5a7b977d8338415779826
nfmohit-wpmudev <nfmohit49@gmail.com>
2020-05-07 22:01:51 +0600

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+/**
+ * ESLint configuration file.
+ *
+ * @author Nahid Ferdous Mohit.
+ * @since  1.0.0
+ */
+
+/*
+ * Dependencies
+ */
+const wordpressPrettierConfig = require( '@wordpress/prettier-config' );
+
+/*
+ * ESLint configuration
+ */
+module.exports = {
+	extends: [
+		'airbnb',
+		'plugin:@wordpress/eslint-plugin/recommended',
+		'prettier'
+	],
+	plugins: [ 'prettier' ],
+	rules: {
+		'prettier/prettier': [
+			'error',
+			{
+				...wordpressPrettierConfig,
+				trailingComma: 'none',
+				overrides: [
+					{
+						files: '*.html',
+						options: {
+							parser: 'html'
+						}
+					}
+				]
+			}
+		],
+		'max-len': [ 'error', 80 ],
+		'no-console': 'warn',
+		eqeqeq: [ 'error', 'smart' ]
+	},
+	parserOptions: {
+		ecmaVersion: 2018
+	},
+	env: {
+		browser: true,
+		node: true,
+		es6: true,
+		commonjs: true
+	}
+};
---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Main package file.
+ *
+ * @author Nahid Ferdous Mohit.
+ * @since  1.0.0
+ */
+
+/*
+ * Export the package
+ */
+module.exports = require( './.eslintrc' );
